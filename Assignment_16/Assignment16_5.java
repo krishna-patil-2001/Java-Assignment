4f4d47cc8f7c0009fd05e1121cd6e9eef8926160
krishna-patil-2001 <dhawalekrushna0@gmail.com>
2025-11-21 18:33:40 +0530

    Add files via upload

---
+++ Assignment_16/Assignment16_5.java
@@ -0,0 +1,60 @@
+/* 5. Accept number of rows and number of columns from user and display below patern. 
+Input: iRow = 4  iCol = 4
+
+Output: 1  2  3  4
+           2  3  4
+              3  4
+                 4
+*/
+
+import java.util.Scanner;
+
+class Pattern
+{
+   int iRow;
+   int iCol;
+
+   Pattern(int A, int B)
+   {
+     this.iRow = A;
+     this.iCol = B;
+   }
+
+   void Display()
+   {
+      for(int i = 1; i <= iRow; i++)
+      {
+         for(int j = 1; j <= iCol; j++)
+         {
+             if(i > j)
+             {
+                System.out.print("   ");
+             }
+             else
+             {
+                System.out.print(j+"  ");
+             }
+         }
+
+         System.out.println();
+      }
+   }
+}
+
+ class Assignment16_5
+{
+   public static void main(String[] args) 
+   {
+       Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter no.of rows: ");
+        int iValue1 = sobj.nextInt();
+
+        System.out.println("Enter no.of column: ");
+        int iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern(iValue1, iValue2);
+
+        pobj.Display();
+   }
+}
